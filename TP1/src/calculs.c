7fcb62262f72cea7d2fe932cab75453b60806aec
yz78 <159271294+yz78@users.noreply.github.com>
2026-09-18 13:58:02 +0000

    exercice 8

--- TP1/src/calculs.c
+++ TP1/src/calculs.c
@@ -1,1 +1,49 @@
+/* Exercice 1.8 : sélectionner une opération arithmétique ou binaire. */
+#include <stdio.h>
+
+int main(void) {
+	int num1 = 12;
+	int num2 = 5;
+	char op = '+';
+
+	switch (op) {
+		case '+':
+			printf("%d + %d = %d\n", num1, num2, num1 + num2);
+			break;
+		case '-':
+			printf("%d - %d = %d\n", num1, num2, num1 - num2);
+			break;
+		case '*':
+			printf("%d * %d = %d\n", num1, num2, num1 * num2);
+			break;
+		case '/':
+			if (num2 == 0) {
+				printf("Division par zero impossible.\n");
+			} else {
+				printf("%d / %d = %d\n", num1, num2, num1 / num2);
+			}
+			break;
+		case '%':
+			if (num2 == 0) {
+				printf("Modulo par zero impossible.\n");
+			} else {
+				printf("%d %% %d = %d\n", num1, num2, num1 % num2);
+			}
+			break;
+		case '&':
+			printf("%d & %d = %d\n", num1, num2, num1 & num2);
+			break;
+		case '|':
+			printf("%d | %d = %d\n", num1, num2, num1 | num2);
+			break;
+		case '~':
+			printf("~%d = %d\n", num1, ~num1);
+			break;
+		default:
+			printf("Operateur inconnu : %c\n", op);
+			return 1;
+	}
+
+	return 0;
+}
 
