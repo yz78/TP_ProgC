22ee0b0a13e13a39bf8469081b52a593849e8ff0
yz78 <159271294+yz78@users.noreply.github.com>
2026-09-18 13:59:01 +0000

    exercice 10

--- TP1/src/pyramide.c
+++ TP1/src/pyramide.c
@@ -1,1 +1,25 @@
+#include <stdio.h>
+
+int main(void) {
+	int n = 5;
+
+	for (int i = 1; i <= n; i++) {
+		for (int j = 1; j <= n - i; j++) {
+			putchar(' ');
+		}
+
+		for (int j = 1; j <= i; j++) {
+			printf("%d", j);
+		}
+
+		for (int j = i - 1; j >= 1; j--) {
+			printf("%d", j);
+		}
+
+		putchar('\n');
+	}
+
+	printf("Generation de la pyramide terminee.\n");
+	return 0;
+}
 
