65c0d371ab7a773a9f3eb87d455b345f26ec7bf7
yz78 <159271294+yz78@users.noreply.github.com>
2026-09-18 13:35:41 +0000

    exercice 3

--- TP1/src/sizeof_types.c
+++ TP1/src/sizeof_types.c
@@ -1,1 +1,31 @@
+#include <stdio.h>
 
+int main(void) {
+    /* Types caractères */
+    printf("char : %zu octet(s)\n", sizeof(char));
+    printf("signed char : %zu octet(s)\n", sizeof(signed char));
+    printf("unsigned char : %zu octet(s)\n\n", sizeof(unsigned char));
+
+    /* Types entiers courts */
+    printf("short : %zu octet(s)\n", sizeof(short));
+    printf("unsigned short : %zu octet(s)\n\n", sizeof(unsigned short));
+
+    /* Types entiers standards */
+    printf("int : %zu octet(s)\n", sizeof(int));
+    printf("unsigned int : %zu octet(s)\n\n", sizeof(unsigned int));
+
+    /* Types entiers longs */
+    printf("long int : %zu octet(s)\n", sizeof(long int));
+    printf("unsigned long int : %zu octet(s)\n\n", sizeof(unsigned long int));
+
+    /* Types entiers très longs */
+    printf("long long int : %zu octet(s)\n", sizeof(long long int));
+    printf("unsigned long long int : %zu octet(s)\n\n", sizeof(unsigned long long int));
+
+    /* Types à virgule flottante */
+    printf("float : %zu octet(s)\n", sizeof(float));
+    printf("double : %zu octet(s)\n", sizeof(double));
+    printf("long double : %zu octet(s)\n", sizeof(long double));
+
+    return 0;
+}
